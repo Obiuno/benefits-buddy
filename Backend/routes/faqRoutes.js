0a50b83ae50be4a6eb413877b8e8cf09a4616934
saniatalha <saniatalha0519@gmail.com>
2026-04-16 12:42:41 +0100

    New changes

--- Backend/routes/faqRoutes.js
+++ Backend/routes/faqRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { apiReference } from "@scalar/express-api-reference";
-import { specs } from "./config/swagger.js";
+import { specs } from "../config/swagger.js";
 import faqsController from "../controllers/faqsController.js";
 
 const faqsRouter = Router();
